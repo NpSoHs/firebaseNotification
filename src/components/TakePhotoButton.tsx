f132607fb29eed4c9f14335af591c5b944a60c2d
torrak <156417791+torrak@users.noreply.github.com>
2024-12-02 09:23:04 +0700

    final mai

--- src/components/TakePhotoButton.tsx
+++ src/components/TakePhotoButton.tsx
@@ -1,21 +1,26 @@
+"use client";
 import { database } from "@/app/firebaseConfig";
 import { useFirebaseData } from "@/utils/useFirebaseData";
 import { onValue, ref, set } from "firebase/database";
-import { useEffect, useState } from "react";
-
 
 const TakePhotoButton = () => {
-    const takePhoto = () => {
-        const randomNum = Math.floor(Math.random() * 50000);
-        const photoUrl = `http://192.168.1.196/capture?_cb=${randomNum}`;
-        const photoUrlRef = ref(database, "/doorbell/photoResponse");
-        set(photoUrlRef, photoUrl);
-    };
-    return (
-        <button onClick={takePhoto} className="bg-gray-300 flex-1 py-2 rounded text-black text-lg">
-            take a photo
-        </button>
-    )
-}
+  const photoUrlString = useFirebaseData("/doorbell/photoResponse");
+  const takePhoto = () => {
+    const randomNum = Math.floor(Math.random() * 50000);
+    const photoUrlCut = photoUrlString.substring(0, 20);
+    console.log(photoUrlCut);
+    const photoUrl = `${photoUrlCut}/capture?_cb=${randomNum}`;
+    const photoUrlRef = ref(database, "/doorbell/photoResponse");
+    set(photoUrlRef, photoUrl);
+  };
+  return (
+    <button
+      onClick={takePhoto}
+      className="bg-gray-300 flex-1 py-2 rounded text-black text-lg"
+    >
+      take a photo
+    </button>
+  );
+};
 
-export default TakePhotoButton
+export default TakePhotoButton;
